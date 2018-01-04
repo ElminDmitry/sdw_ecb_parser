640c30b1f59aaf0d9c6ba3b22e4c746caceda53c
Unknown <elmin.mailbox@gmail.com>
2018-01-04 15:58:03 +0200

    Add csv dowloader

---
+++ lib/interactors/download/amounts_csv.rb
@@ -0,0 +1,13 @@
+module Downloader
+  class AmountsCsv
+    URL = 'https://sdw.ecb.europa.eu/quickview.do?SERIES_KEY=120.EXR.D.USD.EUR.SP00.A'
+
+    private
+
+    def self.file_object
+      html = open(URL)
+      doc = Nokogiri::HTML(html)
+      open(doc.css('div.exportOptions a').map { |link| link['href'] }[1])
+    end
+  end
+end

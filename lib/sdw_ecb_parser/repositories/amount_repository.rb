af5a498d2d49a479a52d1e05edc3ffd910077036
Unknown <elmin.mailbox@gmail.com>
2018-01-04 18:10:21 +0200

    Add one amout entering

--- lib/sdw_ecb_parser/repositories/amount_repository.rb
+++ lib/sdw_ecb_parser/repositories/amount_repository.rb
@@ -5,6 +5,6 @@ class AmountRepository < Hanami::Repository
   end
 
   def exchange(value, dates)
-    amounts.where { (period > dates.min) & (period < dates.max) }.to_a.map { |amount| value.to_i * amount.rate.round(2) }
+    amounts.where { (period >= dates.min) & (period <= dates.max) }.to_a.map { |amount| value.to_i * amount.rate.round(2) }
   end
 end
